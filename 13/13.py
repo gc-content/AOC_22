812fad6b56dd09d2032d57d8650fe85a2e92e590
Piotr <kopiamu@gmail.com>
2022-12-15 14:56:00 +0100

    13 cleanup + part2

--- 13/13.py
+++ 13/13.py
@@ -1,4 +1,36 @@
 import ast 
+from collections import deque
+
+
+def compare(a,b):
+	if type(a) == int and type(b) == int:
+		if a > b:
+			return 0 
+		elif a  < b:
+			return 1
+	elif type(a) == list and type(b) == int:
+		return compare(a, [b])
+	elif type(a) == int and type(b) == list:
+		return compare([a], b)
+	elif type(a) == list and type(b) == list:
+		if len(a) == 0 and len(b) != 0 :
+			return 1
+		elif len(b) == 0 and len(a)!=0:
+			return 0
+		n = min(len(a),len(b))
+		lst = []
+		for idx in range(n):
+			lst.append(compare(a[idx], b[idx]))
+		lst = [k for k in lst if k!=None]
+		if len(lst) == 0:
+			if len(a) < len(b):
+				return 1
+			elif len(a) > len(b): 
+				return 0
+		else:
+			return lst[0]
+
+### PART1 :
 
 pairs = [[]] 
 count = 0
@@ -11,58 +43,9 @@ with open("input.txt") as inp:
 			pairs[count].append(ast.literal_eval(line.strip())) ##danger
 
 
-#print(pairs)
-
-def compare(a,b):
-	print(f"TYPES: {type(a), type(b)}")
-	print(f"PAIR A:{a}")
-	print(f"PAIR B:{b}")
-	if type(a) == int and type(b) == int:
-		print("AAAA")
-		print(a, b)
-		if a > b:  print("1return 0"); return 0 
-		elif a  < b:  print("1return 1"); return 1
-	elif type(a) == list and type(b) == int:
-		print("BBBB")
-		return compare(a, [b])
-	elif type(a) == int and type(b) == list:
-		print("CCCCC")
-		return compare([a], b)
-	elif type(a) == list and type(b) == list:
-		print("DDDDD")
-		if len(a) == 0 and len(b) != 0 : print("2return 1"); return 1
-		elif len(b) == 0 and len(a)!=0: print("2return 0"); return 0
-		n = min(len(a),len(b))
-		print(f"N: {n}")
-		lst = []
-		for idx in range(n):
-			print(f"IDX:{idx}")
-			lst.append(compare(a[idx], b[idx]))
-		print(f"LST0:{lst}")
-		lst = [k for k in lst if k!=None]
-		print(f"LST: {lst}")
-		if len(lst) == 0:
-			print("ZERO")
-			if len(a) < len(b):
-				print("3return 0")
-				return 1
-			elif len(a) > len(b): print("3return 1"); return 0
-		else:
-			print(f"LST2: {lst}, {lst[0]}")
-			return lst[0]
-
-
-
-#print(type(pairs[0][1]))
 aa = []
 for idx, pair in enumerate(pairs):
-	print(f"PAIR {idx}")
-#	for idx2, p in enumerate(pair[1][0]):
-#		print(f"P1:{pair[0][0][idx2]}")
-#		print(f"P1:{pair[1][0][idx2]}")
 	a = compare(pair[0],pair[1])
-	print("aaaaaaaaaaaaaaaaaa")
-	print(a)
 	aa.append(a)
 print(aa)
 
@@ -70,7 +53,31 @@ print(sum([(idx + 1) for idx, a in enumerate(aa) if a == 1]))
 
 
 
+### PART2 : 
+
+with open("input.txt") as inp:
+	dividers = [[[2]],[[6]]]
+	pairs = deque(dividers)
+	b = pairs[0]
+	
+
+	for line in inp:
+		if line.startswith("\n"):
+			continue
+		a = ast.literal_eval(line.strip())
+		idx = 0
+		for idx,n in enumerate(pairs):
+			if compare(a,pairs[idx]) == 1:
+				pairs.insert(idx,a)
+				break
+			elif idx==len(pairs)-1:
+				pairs.append(a)
+				break
 
 
 
+final = 1
+for idx,n in enumerate(pairs):
+	if n in dividers: final *= idx+1    ## could be done on the fly
+print(final)
 
